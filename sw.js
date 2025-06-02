7d6389e5010aee219e96bd20d0253988cf1f877e
RuokGamerz <79328354+HACKTEK90@users.noreply.github.com>
2025-06-02 20:13:44 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,26 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('reminder-cache').then(cache => {
+      return cache.addAll([
+        'index.html',
+        'script.js',
+        'style.css',
+        'manifest.json',
+        'icon.png'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
